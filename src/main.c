7b802907102ea071bda4c9446f86f85e00d49ec1
Kvu23 <kaushik.chauhan23@gmail.com>
2019-04-02 23:32:11 +0530

    Changed main.c

--- src/main.c
+++ src/main.c
@@ -2,8 +2,7 @@
 
 int main(int argc, char const *argv[])
 {
-    /* code */  
-        int i = 10;
-        printf("i = %d\n\n", i);
-    return 0;
+	/* code */  
+	printf("Hello World\n\n");
+	return 0;
 }
